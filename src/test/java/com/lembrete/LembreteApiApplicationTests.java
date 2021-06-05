b1bedfa7ffe5f8353c9924ba121bfdd21d3685ec
Dherkyan Ribeiro <dherkyan@gmail.com>
2021-06-04 23:07:43 -0300

    Initial Commit

---
+++ src/test/java/com/lembrete/LembreteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lembrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LembreteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
